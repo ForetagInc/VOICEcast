0ee0eed2e44e36e58435fdc218af5142d1f7baf3
itzezc <chirub@foretag.co>
2020-10-29 20:06:24 +0530

    Audio w/ FFMPEG

---
+++ source/audio.cr
@@ -0,0 +1,27 @@
+module Shoutcast::Audio
+	extend self
+
+	def ffmpeg_mp3_cmd(file : Path) : String
+		%<ffmpeg -i "#{file}" -vn -f mp3 - >
+	end
+
+	def audio_pipe(file : Path)
+		args = ffmpeg_mp3_cmd file
+		puts args
+		Process.run args, shell: true do | process |
+			sleep 0.1
+			if process.terminated?
+				status = process.wait 
+				return if status.success?
+				exit_code = if status.normal_exit?
+					"exit code" + status.exit_code.to_s
+				else
+					"signal" + status.exit_code.to_s
+				end
+				raise "command #{args} failed with #{exit_code}"
+			else
+				yield process.output
+			end
+		end 
+	end
+end
